16b924981f9b25ec9f8ec37636f8d95fb91f9218
Jerry Hanks <jerryhanksokafor@gmail.com>
2020-09-23 17:14:41 +0100

    initial commit

---
+++ NavigationMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationMenu
+//
+//  Created by Jerry Hanks on 23/09/2020.
+//  Copyright © 2020 Jerry Okafor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
